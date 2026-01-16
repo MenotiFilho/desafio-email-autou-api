238b76f006260385689373287367e81c66399a64
Menoti Borri Filho <menotimfilho@gmail.com>
2026-01-16 10:48:08 -0300

    mudancas no tailwind dos botoes

--- frontend/src/components/ui/Tabs.jsx
+++ frontend/src/components/ui/Tabs.jsx
@@ -2,14 +2,14 @@ import { Upload, FileText } from 'lucide-react';
 
 export function Tabs({ activeTab, setActiveTab }) {
     return (
-        <div className="flex p-1 bg-slate-900 rounded-lg mb-6 border border-slate-700">
+        <div className="flex p-1  rounded-lg mb-6 border border-slate-700">
             {['file', 'text'].map((tab) => (
                 <button
                     key={tab}
                     onClick={() => setActiveTab(tab)}
                     className={`flex-1 py-2 text-sm font-medium rounded-md flex items-center justify-center gap-2 transition-all ${activeTab === tab
-                        ? 'bg-slate-700 text-white shadow-sm'
-                        : 'text-slate-400 hover:text-slate-200'
+                        ? '!bg-indigo-600 !text-white shadow-md'
+                        : 'text-slate-400 hover:text-slate-200 hover:bg-slate-800/50'
                         }`}
                 >
                     {tab === 'file' ? (
